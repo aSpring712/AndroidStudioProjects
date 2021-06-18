cd80009037432d03f2c7ce1088fe59593492b7df
aSpring <rnjsqhal2001@naver.com>
2021-06-18 09:14:22 +0900

    안드로이드 프로젝트

---
+++ chapter_04/app/src/main/java/com/example/chapter_04/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.chapter_04;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+//        // 0.XML 파일을 연결하다
+//        setContentView(R.layout.activity_main);
+//
+//        EditText edt1; // 1. Java 객체를 만든다(위젯View객체)
+//        edt1 = findViewById(R.id.edt1); // 2. XML 객체를 Java 객체에 연결(바인딩)
+//
+//        Button btn1 = findViewById(R.id.btn1);
+//        TextView tv1 = findViewById(R.id.tv1);
+//
+//        btn1.setOn
+
+//        setContentView(R.layout.ex04_01);
+//        setContentView(R.layout.ex04_02);
+//        setContentView(R.layout.ex04_03);
+//        setContentView(R.layout.ex04_04);
+//        setContentView(R.layout.ex04_05);
+//        setContentView(R.layout.ex04_06);
+//        setContentView(R.layout.ex04_07);
+//        setContentView(R.layout.ex04_08);
+//        setContentView(R.layout.ex04_09);
+//        setContentView(R.layout.ex04_10);
+//        setContentView(R.layout.ex04_11);
+//        setContentView(R.layout.ex04_12);
+//        setContentView(R.layout.ex04_13);
+//        setContentView(R.layout.ex04_14);
+//        setContentView(R.layout.ex04_15);
+//        setContentView(R.layout.ex04_16);
+//        setContentView(R.layout.ex04_17);
+//        setContentView(R.layout.ex04_18);
+//        setContentView(R.layout.ex04_19);
+//        setContentView(R.layout.ex04_20);
+//        setContentView(R.layout.ex04_21);
+//        setContentView(R.layout.ex04_22);
+//        setContentView(R.layout.ex04_23);
+//        setContentView(R.layout.ex04_24);
+//        setContentView(R.layout.ex04_25);
+//        setContentView(R.layout.ex04_26);
+//        setContentView(R.layout.ex04_27);
+//        setContentView(R.layout.ex04_28);
+//        setContentView(R.layout.ex04_29);
+        setContentView(R.layout.ex4_2);
+
+    }
+}
